880a7e189ac5ed9f3f3910a90c15465ee5d79408
Matt George <mgeorge@stratasan.com>
2013-06-07 11:14:45 -0500

    first additions for testing

---
+++ tests/test_column_loading.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+from sqlalchemy.types import NullType, VARCHAR
+from redshift_sqlalchemy.dialect import RedshiftDialect
+class TestColumnReflection(TestCase):
+    def test_varchar_as_nulltype(self):
+        """
+        Varchar columns with no length should be considered NullType columns
+        """
+        dialect = RedshiftDialect()
+        column_info = dialect._get_column_info(
+            'Null Column',
+            'character varying', None, False, {}, {}, 'default'
+        )
+        assert isinstance(column_info['type'], NullType)
+        column_info_1 = dialect._get_column_info(
+            'character column',
+            'character varying(30)', None, False, {}, {}, 'default'
+        )
+        assert isinstance(column_info_1['type'], VARCHAR)
