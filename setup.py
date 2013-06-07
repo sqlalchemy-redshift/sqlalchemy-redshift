0a038a4d97cb2e220d8f5d80e2e6750c2e99a858
Matt George <mgeorge@stratasan.com>
2013-06-07 11:19:18 -0500

    adding sqlalchemy as a dependency

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     license="MIT",
     url='https://github.com/binarydud/redshift_sqlalchemy',
     packages=['redshift_sqlalchemy'],
-    install_requires=['psycopg2 >= 2.5'],
+    install_requires=['psycopg2 == 2.5', 'SQLAlchemy==0.8.0b2'],
     include_package_data=True,
     zip_safe=False,
     classifiers=[
