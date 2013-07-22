ee8c6d3b9a30b22d3b08223798c7357408e50eeb
Jason Myers <jason@jasonamyers.com>
2013-07-22 13:57:58 -0500

    Added Documentation Strings
    Signed-off-by: Jason Myers <jason@jasonamyers.com>

--- tests/test_unload_from_select.py
+++ tests/test_unload_from_select.py
@@ -7,12 +7,16 @@ from redshift_sqlalchemy.dialect import RedshiftDialect, UnloadFromSelect, visit
 
 class TestUnloadFromSelect(TestCase):
     def setUp(self):
+        ''' Sets up a in memory sqlite instance with a table
+        '''
         self.metadata = MetaData()
         self.t1 = Table('t1', self.metadata, Column('id', Integer, primary_key=True), Column('name', String))
         self.engine = create_engine('sqlite:///:memory:', echo=True)
         self.metadata.create_all(self.engine)
 
     def test_basic_unload_case(self):
+        ''' Tests that the simplest type of UnloadFromSelect works
+        '''
         expected_result = "unload ('SELECT count(t1.id) AS count_1 \nFROM t1') to 'cookies' credentials 'aws_access_key_id=cookies;aws_secret_access_key=cookies'"
         insert = UnloadFromSelect(select([func.count(self.t1.c.id)]), 'cookies', 'cookies', 'cookies')
         self.assertEqual(expected_result, str(insert))
