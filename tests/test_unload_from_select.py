b9b1ccecd6c9e76c3230f7a65da75540d47232fc
Jason Myers <jason@jasonamyers.com>
2013-07-24 15:13:53 -0500

    Added allowoverwrite to unload
    Signed-off-by: Jason Myers <jason@jasonamyers.com>

--- tests/test_unload_from_select.py
+++ tests/test_unload_from_select.py
@@ -14,6 +14,6 @@ class TestUnloadFromSelect(TestCase):
     def test_basic_unload_case(self):
         ''' Tests that the simplest type of UnloadFromSelect works
         '''
-        expected_result = "unload ('SELECT count(t1.id) AS count_1 \nFROM t1') to 'cookies' credentials 'aws_access_key_id=cookies;aws_secret_access_key=cookies' delimiter ',' addquotes"
+        expected_result = "unload ('SELECT count(t1.id) AS count_1 \nFROM t1') to 'cookies' credentials 'aws_access_key_id=cookies;aws_secret_access_key=cookies' delimiter ',' addquotes allowoverwrite"
         insert = UnloadFromSelect(select([func.count(self.t1.c.id)]), 'cookies', 'cookies', 'cookies')
         self.assertEqual(expected_result, str(insert))
