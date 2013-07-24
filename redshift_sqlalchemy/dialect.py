cfa1a6fcf9ae7572a00c572e31d782490149ad85
Jason Myers <jason@jasonamyers.com>
2013-07-24 15:13:53 -0500

    Added allowoverwrite to unload
    Signed-off-by: Jason Myers <jason@jasonamyers.com>

--- redshift_sqlalchemy/dialect.py
+++ redshift_sqlalchemy/dialect.py
@@ -82,9 +82,8 @@ def visit_unload_from_select(element, compiler, **kw):
     ''' Returns the actual sql query for the UnloadFromSelect class
     '''
     query = compiler.process(element.select)
-    import pdb; pdb.set_trace()
     query_str = str(query).replace("'", "\'")
-    return "unload ('%(query)s') to '%(bucket)s' credentials 'aws_access_key_id=%(access_key)s;aws_secret_access_key=%(secret_key)s' delimiter ',' addquotes" % {
+    return "unload ('%(query)s') to '%(bucket)s' credentials 'aws_access_key_id=%(access_key)s;aws_secret_access_key=%(secret_key)s' delimiter ',' addquotes allowoverwrite" % {
         'query': compiler.process(element.select, unload_select=True, literal_binds=True),
         'bucket': element.bucket,
         'access_key': element.access_key,
