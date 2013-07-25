b82a85204c6aa45a63f473bcf6367b44129c429e
Matt George <mgeorge@gmail.com>
2013-07-25 10:42:26 -0500

    updating version for fixes to unload clause

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='redshift-sqlalchemy',
-    version='0.3.1',
+    version='0.3.2',
     description='Amazon Redshift Dialect for sqlalchemy',
     long_description=open("README.rst").read(),
     author='Matt George',
