05209cd7ba9d7734f0c5f0c5083f11ed891e1d11
Matt George <mgeorge@gmail.com>
2014-01-14 13:22:34 -0600

    updating version for new requirements update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='redshift-sqlalchemy',
-    version='0.4',
+    version='0.4.1',
     description='Amazon Redshift Dialect for sqlalchemy',
     long_description=open("README.rst").read(),
     author='Matt George',
