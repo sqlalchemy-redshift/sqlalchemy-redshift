2bb1dab49d9497aec4db441c834e9993a43f8072
Aron Kyle <aron.kyle@lightspeedretail.com>
2014-04-21 11:51:31 -0400

    Small setup updates.
    Adding a setup.cfg file
    Adding pytest support to setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
 from setuptools import setup
+from setuptools.command.test import test as TestCommand
+import sys
+
+class PyTest(TestCommand):
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
 
 setup(
     name='redshift-sqlalchemy',
@@ -11,6 +19,9 @@ setup(
     url='https://github.com/binarydud/redshift_sqlalchemy',
     packages=['redshift_sqlalchemy'],
     install_requires=['psycopg2>=2.5', 'SQLAlchemy>=0.8.0'],
+    tests_require=['pytest>=2.5.2'],
+    test_suite="tests",
+    cmdclass = {'test': PyTest},
     include_package_data=True,
     zip_safe=False,
     classifiers=[
