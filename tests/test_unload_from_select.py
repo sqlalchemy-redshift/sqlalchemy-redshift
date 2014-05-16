d3cc3998868baf4993165a78a698c0bb9ff334e1
Jason Myers <jason@jasonamyers.com>
2014-05-16 09:46:47 -0500

    Adding option for parallel to unload command
    Signed-off-by: Jason Myers <jason@jasonamyers.com>

--- tests/test_unload_from_select.py
+++ tests/test_unload_from_select.py
@@ -14,6 +14,13 @@ class TestUnloadFromSelect(TestCase):
     def test_basic_unload_case(self):
         ''' Tests that the simplest type of UnloadFromSelect works
         '''
-        expected_result = "unload ('SELECT count(t1.id) AS count_1 \nFROM t1') to 'cookies' credentials 'aws_access_key_id=cookies;aws_secret_access_key=cookies' delimiter ',' addquotes allowoverwrite"
+        expected_result = "unload ('SELECT count(t1.id) AS count_1 \nFROM t1') to 'cookies' credentials 'aws_access_key_id=cookies;aws_secret_access_key=cookies' delimiter ',' addquotes allowoverwrite parallel on"
         insert = UnloadFromSelect(select([func.count(self.t1.c.id)]), 'cookies', 'cookies', 'cookies')
+        self.assertEqual(expected_result, str(insert))
+
+    def test_parallel_off_unload_case(self):
+        ''' Tests that UnloadFromSelect handles parallel being set to off
+        '''
+        expected_result = "unload ('SELECT count(t1.id) AS count_1 \nFROM t1') to 'cookies' credentials 'aws_access_key_id=cookies;aws_secret_access_key=cookies' delimiter ',' addquotes allowoverwrite parallel off"
+        insert = UnloadFromSelect(select([func.count(self.t1.c.id)]), 'cookies', 'cookies', 'cookies', 'off')
         self.assertEqual(expected_result, str(insert))
