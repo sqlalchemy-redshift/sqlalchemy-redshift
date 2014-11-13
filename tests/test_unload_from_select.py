902246cad39fcfc6057ea86c8db0d17b082c6e95
Robert Engel <rengel@hearsaycorp.com>
2014-11-13 04:06:24 +0000

    Fix tests for CopyCommand / UnloadCommand

--- tests/test_unload_from_select.py
+++ tests/test_unload_from_select.py
@@ -14,13 +14,21 @@ class TestUnloadFromSelect(TestCase):
     def test_basic_unload_case(self):
         ''' Tests that the simplest type of UnloadFromSelect works
         '''
-        expected_result = "unload ('SELECT count(t1.id) AS count_1 \nFROM t1') to 'cookies' credentials 'aws_access_key_id=cookies;aws_secret_access_key=cookies' delimiter ',' addquotes allowoverwrite parallel on"
-        insert = UnloadFromSelect(select([func.count(self.t1.c.id)]), 'cookies', 'cookies', 'cookies')
-        self.assertEqual(expected_result, str(insert))
+        expected_result = "UNLOAD ('SELECT count(t1.id) AS count_1 \nFROM t1') TO 's3://bucket/key'\n           " \
+                          "CREDENTIALS 'aws_access_key_id=cookies;aws_secret_access_key=cookies;token=cookies'\n" \
+                          "           DELIMITER ','\n           ADDQUOTES\n           ALLOWOVERWRITE\n          " \
+                          " PARALLEL ON;"
+        insert = UnloadFromSelect(select([func.count(self.t1.c.id)]), 's3://bucket/key', 'cookies', 'cookies',
+                                  'cookies')
+        self.assertEqual(expected_result, str(insert).strip())
 
     def test_parallel_off_unload_case(self):
         ''' Tests that UnloadFromSelect handles parallel being set to off
         '''
-        expected_result = "unload ('SELECT count(t1.id) AS count_1 \nFROM t1') to 'cookies' credentials 'aws_access_key_id=cookies;aws_secret_access_key=cookies' delimiter ',' addquotes allowoverwrite parallel off"
-        insert = UnloadFromSelect(select([func.count(self.t1.c.id)]), 'cookies', 'cookies', 'cookies', 'off')
-        self.assertEqual(expected_result, str(insert))
+        expected_result = "UNLOAD ('SELECT count(t1.id) AS count_1 \nFROM t1') TO 's3://bucket/key'\n           " \
+                          "CREDENTIALS 'aws_access_key_id=cookies;aws_secret_access_key=cookies;token=cookies'\n" \
+                          "           DELIMITER ','\n           ADDQUOTES\n           ALLOWOVERWRITE\n          " \
+                          " PARALLEL OFF;"
+        insert = UnloadFromSelect(select([func.count(self.t1.c.id)]), 's3://bucket/key', 'cookies', 'cookies',
+                                  'cookies', {'parallel': 'OFF'})
+        self.assertEqual(expected_result, str(insert).strip())
