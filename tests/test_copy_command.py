b5d60e8629453eb6a5790fb328ea67b8f4034777
Robert Engel <rengel@hearsaycorp.com>
2014-11-13 04:06:24 +0000

    Fix tests for CopyCommand / UnloadCommand

--- tests/test_copy_command.py
+++ tests/test_copy_command.py
@@ -1,18 +1,18 @@
 from unittest import TestCase
-from sqlalchemy import Table, Column, Integer, String, MetaData
-from sqlalchemy.sql import select, func
 from redshift_sqlalchemy.dialect import CopyCommand
 
 
 class TestCopyCommand(TestCase):
     def setUp(self):
-        ''' Sets up a table and associate meta data for the test queries to build against
-        '''
         pass
 
     def test_basic_copy_case(self):
-        ''' Tests that the simplest type of UnloadFromSelect works
+        ''' Tests that the simplest type of CopyCommand works
         '''
-        expected_result = "copy t1 from 's3://mybucket/data/listing/' credentials 'aws_access_key_id=cookies;aws_secret_access_key=cookies'"
-        insert = CopyCommand('t1', 's3://mybucket/data/listing/', 'cookies', 'cookies')
-        self.assertEqual(expected_result, str(insert))
+        expected_result = "COPY schema1.t1 FROM 's3://mybucket/data/listing/'\n" \
+                          "           CREDENTIALS 'aws_access_key_id=cookies;aws_secret_access_key=cookies'\n" \
+                          "           CSV\n           TRUNCATECOLUMNS\n           DELIMITER ','\n" \
+                          "           IGNOREHEADER 0\n           NULL '---'\n           EMPTYASNULL\n" \
+                          "           BLANKSASNULL;"
+        insert = CopyCommand('schema1', 't1', 's3://mybucket/data/listing/', 'cookies', 'cookies')
+        self.assertEqual(expected_result, str(insert).strip())
