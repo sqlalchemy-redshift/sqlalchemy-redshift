c90d820690484008f12d123c4d7930471934a39e
Robert Engel <rengel@hearsaycorp.com>
2014-12-02 21:56:30 +0000

    More options for UnloadCommand and more resilient test cases

--- tests/test_copy_command.py
+++ tests/test_copy_command.py
@@ -1,18 +1,23 @@
 from unittest import TestCase
 from redshift_sqlalchemy.dialect import CopyCommand
+import re
 
 
 class TestCopyCommand(TestCase):
+
     def setUp(self):
         pass
 
     def test_basic_copy_case(self):
-        ''' Tests that the simplest type of CopyCommand works
         '''
-        expected_result = "COPY schema1.t1 FROM 's3://mybucket/data/listing/'\n" \
-                          "           CREDENTIALS 'aws_access_key_id=cookies;aws_secret_access_key=cookies'\n" \
-                          "           CSV\n           TRUNCATECOLUMNS\n           DELIMITER ','\n" \
-                          "           IGNOREHEADER 0\n           NULL '---'\n           EMPTYASNULL\n" \
-                          "           BLANKSASNULL;"
-        insert = CopyCommand('schema1', 't1', 's3://mybucket/data/listing/', 'cookies', 'cookies')
-        self.assertEqual(expected_result, str(insert).strip())
+            Tests that the simplest type of CopyCommand works
+        '''
+        expected_result = re.sub(r'\s+', ' ',
+                                  "COPY schema1.t1 FROM 's3://mybucket/data/listing/' "
+                                  "CREDENTIALS 'aws_access_key_id=cookies;aws_secret_access_key=cookies' "
+                                  "CSV TRUNCATECOLUMNS DELIMITER ',' IGNOREHEADER 0 EMPTYASNULL BLANKSASNULL;").strip()
+        copy = CopyCommand('schema1', 't1', 's3://mybucket/data/listing/', 'cookies', 'cookies')
+
+        copy_str = re.sub(r'\s+', ' ', str(copy)).strip()
+
+        self.assertEqual(expected_result, copy_str)
