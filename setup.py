cf887d93ebc5314d70b99a6225c311ad6c434bb3
Thomas Grainger <tom@yplanapp.com>
2015-10-08 13:40:29 +0100

    Back to development: 0.3.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ history = open('CHANGES.rst').read().replace('.. :changelog:', '')
 
 setup(
     name='sqlalchemy-redshift',
-    version='0.3.1',
+    version='0.3.2.dev0',
     description='Amazon Redshift Dialect for sqlalchemy',
     long_description=readme + '\n\n' + history,
     author='Matt George',
