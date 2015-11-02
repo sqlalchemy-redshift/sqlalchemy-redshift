aed0bb9c309022a11866226699f3e75e995938d8
Jeff Klukas <jeff@klukas.net>
2015-11-02 12:09:17 -0500

    Add trove classifiers for the specific supported python versions

--- setup.py
+++ setup.py
@@ -29,6 +29,13 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
     ],
     entry_points={
         'sqlalchemy.dialects': [
