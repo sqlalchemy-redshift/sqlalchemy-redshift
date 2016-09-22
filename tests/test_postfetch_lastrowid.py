4275c7752270215cd7f9ca1853c33331c7226f00
Thomas Grainger <tom.grainger@procensus.com>
2016-09-22 22:54:21 +0100

    Revert "Fix integration tests."
    This reverts commit 6a0462755ad0e1d33a97821d8240c461a5c7b807.

--- tests/test_postfetch_lastrowid.py
+++ tests/test_postfetch_lastrowid.py
@@ -3,7 +3,8 @@ from rs_sqla_test_utils import models
 
 def test_postfetch_lastrowid(redshift_session):
     session = redshift_session
-    examples = [models.PostfetchExample(some_int=i) for i in range(5)]
-    session.add_all(examples)
+    for i in range(5):
+        postfetchExample = models.PostfetchExample(some_int=i)
+        session.add(postfetchExample)
     session.flush()
-    assert sorted(pfe.id for pfe in examples) == [0, 1, 2, 3, 4]
+    assert postfetchExample.id == 4
