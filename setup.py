2b987fdf39536282eee95605ad2297497d90812b
Jeff Klukas <jeff@klukas.net>
2018-01-17 13:25:01 -0500

    Fix incompatibility of reflection with SQA 1.2.0 (#139)
    * Fix incompatibility of reflection with SQA 1.2.0
    
    Addresses https://github.com/sqlalchemy-redshift/sqlalchemy-redshift/issues/138

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
         'psycopg2>=2.5',
          # requires sqlalchemy.sql.base.DialectKWArgs.dialect_options, new in
          # version 0.9.2
-        'SQLAlchemy>=0.9.2',
+        'SQLAlchemy>=0.9.2,<2.0.0',
     ],
     extras_require={
         ':python_version < "3.4"': 'enum34 >= 1.1.6, < 2.0.0'
