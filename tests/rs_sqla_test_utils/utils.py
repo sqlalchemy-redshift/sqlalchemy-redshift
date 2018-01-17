3cd61fd40a99ad1c28638509fae9ea8201679380
Jeff Klukas <jeff@klukas.net>
2018-01-17 15:29:56 -0500

    Fix various bugs in tests (#140)
    Our tests have drifted out of sync with changes in Redshift and SQLAlchemy.
    
    Fixes https://github.com/sqlalchemy-redshift/sqlalchemy-redshift/issues/131

--- tests/rs_sqla_test_utils/utils.py
+++ tests/rs_sqla_test_utils/utils.py
@@ -5,8 +5,9 @@ from sqlalchemy_redshift import dialect
 
 
 def clean(query):
-    encodings_removed = re.sub(r'\s+ENCODE\s+\w+', '', query)
-    return re.sub(r'\s+', ' ', encodings_removed).strip()
+    query = re.sub(r'\s+ENCODE\s+\w+', '', query)
+    query = re.sub(r'\s+CONSTRAINT\s+[a-zA-Z0-9_".]+', '', query)
+    return re.sub(r'\s+', ' ', query).strip()
 
 
 def compile_query(q):
