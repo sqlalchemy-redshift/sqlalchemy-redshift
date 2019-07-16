db45ef1a06ec79f9a0a10e2aa616ef025f46d6b6
Grzegorz Śliwiński <g.sliwinski@clearcode.cc>
2019-07-16 17:40:37 +0200

    preserve commit and enforce version

--- sqlalchemy_redshift/__init__.py
+++ sqlalchemy_redshift/__init__.py
@@ -1,6 +1,11 @@
-from pkg_resources import get_distribution
+from pkg_resources import get_distribution, parse_version
+
 try:
     import psycopg2  # noqa: F401
+    if get_distribution('psycopg2').parsed_version < parse_version(2.5):
+        raise ImportError('Minimum required version for psycopg2 is 2.5')
+        # requires sqlalchemy.sql.base.DialectKWArgs.dialect_options, new in
+        # version 0.9.2
 except ImportError:
     raise ImportError(
         'No module named psycopg2. Please install either '
