ff16b3ca4b6f43d2db041ac5e51baeed3e964abc
Jeppe Fihl-Pearson <jeppe@tenzer.dk>
2019-10-09 10:44:09 +0100

    Check the version of any of the supported Psycopg2 packages
    A check was introduced in commit 8e0c4857a1c08f257b95d3b1ee5f6eb795d55cdc which
    would check what version of the 'psycopg2' Python (pip) package was installed
    as the dependency was removed from setup.py.
    
    The check would however only check the 'psycopg2' package and not the other two
    supported providers of the psycopg2 module, which meant importing the
    sqlalchemy_redshift module would throw an exception, even though they were
    installed.
    
    This changes the check to check for either of the three supported psycopg2
    packages and throws an exception if any of them fail to validate.

--- sqlalchemy_redshift/__init__.py
+++ sqlalchemy_redshift/__init__.py
@@ -1,14 +1,27 @@
-from pkg_resources import get_distribution, parse_version
+from pkg_resources import DistributionNotFound, get_distribution, parse_version
 
 try:
     import psycopg2  # noqa: F401
-    if get_distribution('psycopg2').parsed_version < parse_version('2.5'):
-        raise ImportError('Minimum required version for psycopg2 is 2.5')
 except ImportError:
     raise ImportError(
         'No module named psycopg2. Please install either '
         'psycopg2 or psycopg2-binary package for CPython '
         'or psycopg2cffi for Pypy.'
+    ) from None
+
+for package in ['psycopg2', 'psycopg2-binary', 'psycopg2cffi']:
+    try:
+        if get_distribution(package).parsed_version < parse_version('2.5'):
+            raise ImportError('Minimum required version for psycopg2 is 2.5')
+        break
+    except DistributionNotFound:
+        pass
+else:
+    raise ImportError(
+        'A module was found named psycopg2, '
+        'but the version of it could not be checked '
+        'as it was neither the Python package psycopg2, '
+        'psycopg2-binary or psycopg2cffi.'
     )
 
 __version__ = get_distribution('sqlalchemy-redshift').version
