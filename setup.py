328cefbc625eafbcfc2fffe3f1bd22ac8204b66e
Jeff Klukas <jeff@klukas.net>
2020-05-27 08:46:39 -0400

    Back to development: 0.7.9

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ history = open('CHANGES.rst').read().replace('.. :changelog:', '')
 
 setup(
     name='sqlalchemy-redshift',
-    version='0.7.8',
+    version='0.7.9.dev0',
     description='Amazon Redshift Dialect for sqlalchemy',
     long_description=readme + '\n\n' + history,
     long_description_content_type='text/x-rst',
