097375fa56772f48091edf6a1f473f5fbff32326
Brooke White <whbrook@amazon.com>
2021-08-18 12:44:34 -0700

    test(dialect): custom dialect class inheritance

--- tests/test_dialects.py
+++ tests/test_dialects.py
@@ -14,10 +14,35 @@ from redshift_sqlalchemy import dialect
     ('redshift+psycopg2', psycopg2.dialect),
     ('redshift+psycopg2cffi', psycopg2cffi.dialect),
 ])
-def test_dialect(name, expected_dialect):
+def test_dialect_inherits_from_sqlalchemy_dialect(name, expected_dialect):
     engine = sa.create_engine(URL(
         drivername=name,
     ))
 
-    assert isinstance(engine.dialect, dialect.RedshiftDialectMixin)
+    assert isinstance(engine.dialect, expected_dialect)
+
+
+@pytest.mark.parametrize('name, expected_dialect', [
+    ('redshift', dialect.PsycopgRedshiftDialectMixin),
+    ('redshift+psycopg2', dialect.PsycopgRedshiftDialectMixin),
+    ('redshift+psycopg2cffi', dialect.PsycopgRedshiftDialectMixin),
+])
+def test_dialect_inherits_from_redshift_mixin(name, expected_dialect):
+    engine = sa.create_engine(URL(
+        drivername=name,
+    ))
+
+    assert isinstance(engine.dialect, expected_dialect)
+
+
+@pytest.mark.parametrize('name, expected_dialect', [
+    ('redshift', dialect.Psycopg2RedshiftDialect),
+    ('redshift+psycopg2', dialect.Psycopg2RedshiftDialect),
+    ('redshift+psycopg2cffi', dialect.Psycopg2CFFIRedshiftDialect),
+])
+def test_dialect_registered_correct_class(name, expected_dialect):
+    engine = sa.create_engine(URL(
+        drivername=name,
+    ))
+
     assert isinstance(engine.dialect, expected_dialect)
