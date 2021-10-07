5597d3d1330f56a0dd263fdaa143e5b79d32e4a0
Jeff Klukas <jeff@klukas.net>
2021-10-07 19:14:58 -0400

    Only test on 3.4 and 3.9

--- setup.py
+++ setup.py
@@ -36,6 +36,8 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     entry_points={
         'sqlalchemy.dialects': [
