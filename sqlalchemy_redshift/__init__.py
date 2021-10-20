01b36da6b730cd278cfb94c56ad2fbacb856aa14
David Szabo <senkisem.xaharmadikon@gmail.com>
2021-10-20 14:05:13 +0200

    Adapt to Redshift new version format

--- sqlalchemy_redshift/__init__.py
+++ sqlalchemy_redshift/__init__.py
@@ -11,6 +11,25 @@ for package in ['psycopg2', 'psycopg2-binary', 'psycopg2cffi']:
 __version__ = get_distribution('sqlalchemy-redshift').version
 
 from sqlalchemy.dialects import registry
+from sqlalchemy.dialect.postgresql.base import PGDialect
+
+def _get_server_version_info(self, connection):
+    v = connection.exec_driver_sql("select pg_catalog.version()").scalar()
+    m = re.match(
+            r".*(?:PostgreSQL|EnterpriseDB) "
+            r"(\d+)\.?(\d+)?(?:\.(\d+))?(?:\.\d+)?(?:devel|beta)?",
+            v,
+    )
+    f not m:
+        if "Redshift" in v: # There is no way of extracting Postgres version from the new version format
+            return (8, 0, 2)
+        raise AssertionError(
+            "Could not determine version from string '%s'" % v
+        )
+    return tuple([int(x) for x in m.group(1, 2, 3) if x is not None])
+
+
+PGDialect._get_server_version_info = _get_server_version_info
 
 registry.register(
     "redshift", "sqlalchemy_redshift.dialect",
