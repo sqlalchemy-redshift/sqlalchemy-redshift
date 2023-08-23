1941fa7da87387ea0e1820648350587142868125
Beto Dealmeida <roberto@dealmeida.net>
2023-08-23 16:05:01 -0700

    chore: add new entry point

--- setup.py
+++ setup.py
@@ -1,28 +1,28 @@
 from setuptools import setup
 
-readme = open('README.rst').read()
-history = open('CHANGES.rst').read().replace('.. :changelog:', '')
+readme = open("README.rst").read()
+history = open("CHANGES.rst").read().replace(".. :changelog:", "")
 
 setup(
-    name='sqlalchemy-redshift',
-    version='0.8.12',
-    description='Amazon Redshift Dialect for sqlalchemy',
-    long_description=readme + '\n\n' + history,
-    long_description_content_type='text/x-rst',
-    author='Matt George',
-    author_email='mgeorge@gmail.com',
-    maintainer='Thomas Grainger',
-    maintainer_email='sqlalchemy-redshift@graingert.co.uk',
+    name="sqlalchemy-redshift-no-spectrum",
+    version="0.8.12",
+    description="Fork of Amazon Redshift Dialect for sqlalchemy without Spectrum support",
+    long_description=readme + "\n\n" + history,
+    long_description_content_type="text/x-rst",
+    author="Matt George",
+    author_email="mgeorge@gmail.com",
+    maintainer="Beto Dealmeida",
+    maintainer_email="beto@preset.io",
     license="MIT",
-    url='https://github.com/sqlalchemy-redshift/sqlalchemy-redshift',
-    packages=['sqlalchemy_redshift', 'redshift_sqlalchemy'],
-    package_data={'sqlalchemy_redshift': ['redshift-ca-bundle.crt']},
-    python_requires='>=3.4',
+    url="https://github.com/preset-io/sqlalchemy-redshift",
+    packages=["sqlalchemy_redshift", "redshift_sqlalchemy"],
+    package_data={"sqlalchemy_redshift": ["redshift-ca-bundle.crt"]},
+    python_requires=">=3.4",
     install_requires=[
         # requires sqlalchemy.sql.base.DialectKWArgs.dialect_options, new in
         # version 0.9.2
-        'SQLAlchemy>=0.9.2,<2.0.0',
-        'packaging',
+        "SQLAlchemy>=0.9.2,<2.0.0",
+        "packaging",
     ],
     classifiers=[
         "Development Status :: 4 - Beta",
@@ -40,11 +40,11 @@ setup(
         "Programming Language :: Python :: 3.9",
     ],
     entry_points={
-        'sqlalchemy.dialects': [
-            'redshift = sqlalchemy_redshift.dialect:RedshiftDialect_psycopg2',
-            'redshift.psycopg2 = sqlalchemy_redshift.dialect:RedshiftDialect_psycopg2',
-            'redshift.psycopg2cffi = sqlalchemy_redshift.dialect:RedshiftDialect_psycopg2cffi',
-            'redshift.redshift_connector = sqlalchemy_redshift.dialect:RedshiftDialect_redshift_connector',
+        "sqlalchemy.dialects": [
+            "redshift.nospectrum = sqlalchemy_redshift.dialect:RedshiftDialect_psycopg2",
+            # "redshift.psycopg2 = sqlalchemy_redshift.dialect:RedshiftDialect_psycopg2",
+            # "redshift.psycopg2cffi = sqlalchemy_redshift.dialect:RedshiftDialect_psycopg2cffi",
+            # "redshift.redshift_connector = sqlalchemy_redshift.dialect:RedshiftDialect_redshift_connector",
         ]
     },
 )
